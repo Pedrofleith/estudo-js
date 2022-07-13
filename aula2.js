448eefba02d5437f4a417ca02acfc8f9592705db
Pedro Fleith <pefleith@gmail.com>
2022-07-13 19:48:43 -0300

    códigos var e conversões

---
+++ aula2.js
@@ -0,0 +1,45 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// var area = altura * comprimento
+// console.log(area)
+
+// let
+
+// let forma = "retângulo";
+// let altura = 5;
+// let comprimento = 7;
+
+// let area;
+
+// if (forma === "retângulo") {
+//     area = altura * comprimento
+// } else
+//     area = (altura * comprimento) / 2 ;
+
+// console.log(area)
+
+
+// const forma = 'triângulo'
+// const altura = 5;
+// const comprimento = 7;
+// let area;
+
+// if (forma === 'quadrado') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+
+// console.log(area)
+
+// let usuário = true;
+// console.log(usuário)
+// usuário = false; 
+// console.log(usuário)
+
+
+console.log(0 == false)
+console.log("" === false)
