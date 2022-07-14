50d42accf73831ab14b60586025469a526472a61
Pedro Fleith <pefleith@gmail.com>
2022-07-13 21:27:55 -0300

    estudando o console e erros

---
+++ console.js
@@ -0,0 +1,13 @@
+// log --> registro
+
+const minhaVar = true;
+
+console.log(245)
+console.log("isso é um teste")
+console.log(minhaVar)
+
+
+// Tratamento de erro
+
+console.log('deu erro!');
+console.error(new Error('deu erro!'));
