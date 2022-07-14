953638d98a4ecc241bd85bb19bd1c5584283e1e7
Pedro Fleith <pefleith@gmail.com>
2022-07-14 12:54:27 -0300

    exemplos de tipos de funções

---
+++ arrow-functions.js
@@ -0,0 +1,23 @@
+function apresentar(nome){
+    return `meu nome é ${nome}`
+}
+
+// função arrow function, pois ela só faz uma ação
+const apresentarArrow = nome => `meu nome é ${nome}`;
+
+// a gente usa parentese pois estamos passando 2 argumentos
+const soma = (num1, num2) => num1 + num2;
+
+// a arrow function sempre vendo como uma const, ela não pode ser nomeado, mas ele sempre vem uma const e o nome da variável que vamos usar
+
+// quando usamos mais de uma linha de instruções, precisamos usar chaves
+                                      //seta (arrow) é diferente do operador de >= (maior igual)
+const somarNumerosPequenos = (num1, num2) => {
+    if (num1 || num2 > 10){
+        return 'somentes números de 1 a 9'
+    }else{
+        return num1 + num2
+    }
+}  
+
+// se comporta como HOISTING do mesmo jeito que expressão de função, ou seja temos que declarar elas no começo do código, pois senão conseguiriamos usar em qualquer lugar como o modo de declaração de função.
---
+++ expressoes-funcoes.js
@@ -0,0 +1,27 @@
+// declaração de função
+// function minhaFuncao(){
+//     // bloco de códio
+// }
+
+// expressão de função
+
+// const soma = function(num1, num2) {return num1 + num2}
+
+// console.log(soma(1,1))
+
+
+// diferença principal ???
+
+console.log(apresentar())
+
+function apresentar(){
+    return 'olá';
+}
+
+console.log(soma())
+const soma = function(num1, num2) {return num1 + num2}
+
+
+// O js lista todas a funções, que chama de : HOISTING
+// funções e var são 'listadas' no topo do arquivo.
+// se a gente usar a funções por expressão a gente tem q 'colocar' essa função no topo do arquivo, pois senão ela não irá rodar
